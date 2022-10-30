c761e01250806dff064486aada40a8c53e11850c
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-10-31 00:28:02 +0100

    tests

--- tests_unit/datasets/test_kd_dataset.py
+++ tests_unit/datasets/test_kd_dataset.py
@@ -23,3 +23,19 @@ class TestKDDataset(unittest.TestCase):
         ds = IndexDataset(size=3)
         with self.assertRaises(UseModeWrapperException):
             _ = ds[0]
+
+    def test_has_wrapper(self):
+        root_ds = IndexDataset(size=3)
+        self.assertFalse(root_ds.has_wrapper(None))
+
+    def test_has_wrapper_type(self):
+        root_ds = IndexDataset(size=3)
+        self.assertFalse(root_ds.has_wrapper_type(None))
+
+    def test_all_wrappers(self):
+        root_ds = IndexDataset(size=3)
+        self.assertEqual([], root_ds.all_wrappers)
+
+    def test_all_wrapper_types(self):
+        root_ds = IndexDataset(size=3)
+        self.assertEqual([], root_ds.all_wrapper_types)
--- tests_unit/datasets/test_kd_wrapper.py
+++ tests_unit/datasets/test_kd_wrapper.py
@@ -1,89 +1,91 @@
 import unittest
 
-from kappadata.datasets.kd_subset import KDSubset
+from kappadata.datasets.kd_wrapper import KDWrapper
 from kappadata.errors import UseModeWrapperException
 from tests_mock.index_dataset import IndexDataset
 
 
-class TestKDSubset(unittest.TestCase):
+class TestKDWrapper(unittest.TestCase):
     def test_getattr_getitem(self):
         root_ds = IndexDataset(size=3)
-        ds = KDSubset(root_ds, indices=[1, 2])
-        self.assertEquals(1, ds.getitem_x(0))
-        self.assertEquals(2, ds.getitem_x(1))
+        ds = KDWrapper(root_ds)
+        self.assertEquals(0, ds.getitem_x(0))
+        self.assertEquals(1, ds.getitem_x(1))
+        self.assertEquals(2, ds.getitem_x(2))
 
     def test_getattr_getitem_recursive(self):
-        root_ds = IndexDataset(size=4)
-        ds = KDSubset(KDSubset(root_ds, indices=[1, 2]), indices=[1])
-        self.assertEquals(2, ds.getitem_x(0))
+        root_ds = IndexDataset(size=2)
+        ds = KDWrapper(KDWrapper(root_ds))
+        self.assertEquals(0, ds.getitem_x(0))
+        self.assertEquals(1, ds.getitem_x(1))
 
     def test_getattr_dataset(self):
         root_ds = IndexDataset(size=3)
-        ds = KDSubset(root_ds, indices=[1, 2])
+        ds = KDWrapper(root_ds)
         self.assertEqual(root_ds, ds.dataset)
 
     def test_getattr_dataset_recursive(self):
         ds0 = IndexDataset(size=3)
-        ds1 = KDSubset(ds0, indices=[1, 2])
-        ds = KDSubset(ds1, indices=[1])
+        ds1 = KDWrapper(ds0)
+        ds = KDWrapper(ds1)
         self.assertEqual(ds1, ds.dataset)
         self.assertEqual(ds0, ds.dataset.dataset)
 
     def test_getattr(self):
-        ds = KDSubset(IndexDataset(size=3), indices=[1, 2])
+        ds = KDWrapper(IndexDataset(size=3))
         self.assertEqual(3, ds.size)
 
     def test_getattr_recursive(self):
-        ds = KDSubset(KDSubset(IndexDataset(size=3), indices=[1, 2]), indices=[0])
+        ds = KDWrapper(KDWrapper(IndexDataset(size=3)))
         self.assertEqual(3, ds.size)
 
     def test_dispose(self):
-        ds = KDSubset(IndexDataset(size=3), indices=[0, 1])
+        ds = KDWrapper(IndexDataset(size=3))
         ds.dispose()
         self.assertTrue(ds.disposed)
 
     def test_dispose_context_manager(self):
-        with KDSubset(IndexDataset(size=3), indices=[0, 1]) as ds:
+        with KDWrapper(IndexDataset(size=3)) as ds:
             pass
         self.assertTrue(ds.disposed)
 
     def test_root_dataset(self):
         root_ds = IndexDataset(size=3)
-        ds = KDSubset(root_ds, indices=[0, 1])
+        ds = KDWrapper(root_ds)
         self.assertEqual(root_ds, ds.root_dataset)
 
     def test_root_dataset_recursive(self):
         root_ds = IndexDataset(size=3)
-        ds = KDSubset(KDSubset(root_ds, indices=[0, 1]), indices=[0])
+        ds = KDWrapper(KDWrapper(root_ds))
         self.assertEqual(root_ds, ds.root_dataset)
 
     def test_getitem(self):
-        ds = KDSubset(IndexDataset(size=3), indices=[0, 1])
+        ds = KDWrapper(IndexDataset(size=3))
         with self.assertRaises(UseModeWrapperException):
             _ = ds[0]
 
     def test_has_wrapper(self):
         root_ds = IndexDataset(size=3)
-        ds = KDSubset(root_ds, indices=[0, 1])
+        ds = KDWrapper(root_ds)
         self.assertFalse(root_ds.has_wrapper(ds))
         self.assertTrue(ds.has_wrapper(ds))
 
     def test_has_wrapper_type(self):
         root_ds = IndexDataset(size=3)
-        ds = KDSubset(root_ds, indices=[0, 1])
-        self.assertFalse(root_ds.has_wrapper_type(KDSubset))
-        self.assertTrue(ds.has_wrapper_type(KDSubset))
+        ds = KDWrapper(root_ds)
+        self.assertFalse(root_ds.has_wrapper_type(KDWrapper))
+        self.assertTrue(ds.has_wrapper_type(KDWrapper))
 
     def test_all_wrappers(self):
         root_ds = IndexDataset(size=3)
-        wrapper1 = KDSubset(root_ds, indices=[0, 1])
-        wrapper2 = KDSubset(wrapper1, indices=[1, 0])
+        wrapper1 = KDWrapper(root_ds)
+        wrapper2 = KDWrapper(wrapper1)
         self.assertEqual([wrapper1], wrapper1.all_wrappers)
         self.assertEqual([wrapper2, wrapper1], wrapper2.all_wrappers)
 
     def test_all_wrapper_types(self):
         root_ds = IndexDataset(size=3)
-        wrapper1 = KDSubset(root_ds, indices=[0, 1])
-        wrapper2 = KDSubset(wrapper1, indices=[1, 0])
-        self.assertEqual([KDSubset], wrapper1.all_wrapper_types)
-        self.assertEqual([KDSubset, KDSubset], wrapper2.all_wrapper_types)
+        wrapper1 = KDWrapper(root_ds)
+        wrapper2 = KDWrapper(wrapper1)
+        self.assertEqual([KDWrapper], wrapper1.all_wrapper_types)
+        self.assertEqual([KDWrapper, KDWrapper], wrapper2.all_wrapper_types)
