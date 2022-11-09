ef199a9f250a97cb024f613cd2da9b27e7015cad
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-11-09 20:15:06 +0100

    Update kd_compose_transform.py

--- kappadata/transforms/base/kd_compose_transform.py
+++ kappadata/transforms/base/kd_compose_transform.py
@@ -1,9 +1,17 @@
+import numpy as np
 from .kd_transform import KDTransform
 from torchvision.transforms import Compose
 
 class KDComposeTransform(KDTransform):
     def __init__(self, transforms):
         self.transforms = transforms
+
+    def set_seed(self, seed):
+        # sample seeds with seed to avoid any potential patterns in child transforms
+        rng = np.random.default_rng(seed)
+        for t in self.transforms:
+            if isinstance(t, KDStochasticTransform):
+                t.set_seed(rng.integers(99999999))
 
     def __call__(self, x, ctx=None):
         if ctx is None:
