bd20e60575a7c99fb351266f43f4d6ebfb049223
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-12-08 01:55:06 +0100

    patchwise shuffle

---
+++ kappadata/transforms/patchwise_shuffle.py
@@ -0,0 +1,14 @@
+from torchvision.transforms.functional import rotate
+
+from .base.kd_stochastic_transform import KDStochasticTransform
+
+
+class PatchwiseShuffle(KDStochasticTransform):
+    def __call__(self, x, ctx=None):
+        c, l, patch_h, patch_w = x.shape
+        # sample rotations
+        permutation = self.rng.permutation(l)
+        if ctx is not None:
+            ctx["permutation"] = permutation
+        x = x[:, permutation]
+        return x
