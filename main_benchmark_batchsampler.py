7b65f88e856e4bff59b7365b64bed22aa1695cce
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-12-23 14:29:31 +0100

    Update main_benchmark_batchsampler.py

--- main_benchmark_batchsampler.py
+++ main_benchmark_batchsampler.py
@@ -25,28 +25,28 @@ def main():
     num_workers = 10
 
     # torch
-    loader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        drop_last=True,
-        num_workers=num_workers,
-        persistent_workers=True,
-    )
-    with kp.Stopwatch() as sw1:
-        for i in range(epochs):
-            print(f"epoch {i+1}")
-            iterator = iter(loader)
-            batch = 0
-            while True:
-                try:
-                    with kp.Stopwatch() as sw2:
-                        next(iterator)
-                    batch += 1
-                    print(f"batch {batch} {sw2.elapsed_seconds}")
-                except StopIteration:
-                    break
-    print(sw1.elapsed_seconds)
+    # loader = DataLoader(
+    #     dataset,
+    #     batch_size=batch_size,
+    #     shuffle=True,
+    #     drop_last=True,
+    #     num_workers=num_workers,
+    #     persistent_workers=True,
+    # )
+    # with kp.Stopwatch() as sw1:
+    #     for i in range(epochs):
+    #         print(f"epoch {i+1}")
+    #         iterator = iter(loader)
+    #         batch = 0
+    #         while True:
+    #             try:
+    #                 with kp.Stopwatch() as sw2:
+    #                     next(iterator)
+    #                 batch += 1
+    #                 print(f"batch {batch} {sw2.elapsed_seconds}")
+    #             except StopIteration:
+    #                 break
+    # print(sw1.elapsed_seconds)
 
     # kd
     sampler = RandomSampler(data_source=dataset)
@@ -62,7 +62,7 @@ def main():
         batches_per_epoch = len(dataset) // batch_size
         for i in range(epochs):
             print(f"epoch {i+1}")
-            for j in batches_per_epoch:
+            for j in range(batches_per_epoch):
                 with kp.Stopwatch() as sw2:
                     next(iterator)
                 print(f"batch {j+1} {sw2.elapsed_seconds}")
