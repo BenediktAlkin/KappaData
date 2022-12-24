64f6da6012d7f10b38de1208e54813c2a2a59315
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-12-24 02:12:13 +0100

    Update test_infinite_batch_sampler.py

--- tests_unit/batch_samplers/test_infinite_batch_sampler.py
+++ tests_unit/batch_samplers/test_infinite_batch_sampler.py
@@ -14,9 +14,16 @@ class TestInfiniteBatchSampler(unittest.TestCase):
             for batch_size in [1, 2, 4]:
                 for drop_last in [True, False]:
                     ds = ModeWrapper(dataset=IndexDataset(size=dataset_size), mode="x")
-                    sampler = RandomSampler(ds)
-                    batch_sampler = InfiniteBatchSampler(sampler=sampler, batch_size=batch_size, drop_last=drop_last)
-                    loader = DataLoader(ds, batch_sampler=batch_sampler)
+                    infinite_sampler = RandomSampler(ds, generator=torch.Generator().manual_seed(5))
+                    batch_sampler = InfiniteBatchSampler(
+                        sampler=infinite_sampler,
+                        batch_size=batch_size,
+                        drop_last=drop_last,
+                    )
+                    infinite_loader = DataLoader(ds, batch_sampler=batch_sampler)
+                    finite_sampler = RandomSampler(ds, generator=torch.Generator().manual_seed(5))
+                    finite_loader = DataLoader(ds, batch_size=batch_size, drop_last=drop_last, sampler=finite_sampler)
+
 
                     if drop_last:
                         batches_per_epoch = dataset_size // batch_size
@@ -24,11 +31,17 @@ class TestInfiniteBatchSampler(unittest.TestCase):
                         batches_per_epoch = (dataset_size + batch_size - 1) // batch_size
 
                     samples = []
-                    iterator = iter(loader)
+                    iterator = iter(infinite_loader)
                     for i in range(n_epochs):
                         for j in range(batches_per_epoch):
                             samples.append(next(iterator).clone())
                     samples = torch.concat(samples)
+                    finite_samples = []
+                    for i in range(n_epochs):
+                        for x in finite_loader:
+                            finite_samples.append(x.clone())
+                    finite_samples = torch.concat(finite_samples)
+                    self.assertEqual(finite_samples.tolist(), samples.tolist())
                     if not drop_last:
                         uniques, counts = samples.unique(return_counts=True)
                         self.assertTrue(torch.all(counts == counts[0]))
