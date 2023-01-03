df207fdaf0e2ecbf3cbbc57ba046524666a4c604
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-03 22:43:07 +0100

    InfiniteBatchSamplerIterator

---
+++ kappadata/batch_samplers/infinite_batch_sampler_iterator.py
@@ -0,0 +1,18 @@
+from .infinite_batch_sampler import InfiniteBatchSampler
+
+class InfiniteBatchSamplerIterator:
+    def __init__(self, dataloader):
+        self.dataloader = dataloader
+
+    def __iter__(self):
+        assert isinstance(self.dataloader.batch_sampler, InfiniteBatchSampler)
+        # adapted from torch.utils.data.sampler.BatchSampler.__len__
+        sampler = self.dataloader.batch_sampler
+        if sampler.drop_last:
+            batches_per_epoch =  len(sampler.sampler) // sampler.batch_size
+        else:
+            batches_per_epoch = (len(sampler.sampler) + sampler.batch_size - 1) // sampler.batch_size
+        for batch_idx, batch in enumerate(self.dataloader):
+            epoch_counter = batch_idx // batches_per_epoch
+            batch_counter = batch_idx % batches_per_epoch
+            yield epoch_counter, batch_counter, batch
--- tests_unit/batch_samplers/test_infinite_batch_sampler.py
+++ tests_unit/batch_samplers/test_infinite_batch_sampler.py
@@ -3,6 +3,7 @@ import unittest
 import torch
 from torch.utils.data import DataLoader, RandomSampler
 from kappadata.batch_samplers.infinite_batch_sampler import InfiniteBatchSampler
+from kappadata.batch_samplers.infinite_batch_sampler_iterator import InfiniteBatchSamplerIterator
 from kappadata.wrappers.mode_wrapper import ModeWrapper
 from tests_util.index_dataset import IndexDataset
 
@@ -20,35 +21,14 @@ class TestInfiniteBatchSampler(unittest.TestCase):
                         batch_size=batch_size,
                         drop_last=drop_last,
                     )
-                    infinite_loader = DataLoader(ds, batch_sampler=batch_sampler)
+                    infinite_loader = InfiniteBatchSamplerIterator(DataLoader(ds, batch_sampler=batch_sampler))
                     finite_sampler = RandomSampler(ds, generator=torch.Generator().manual_seed(5))
                     finite_loader = DataLoader(ds, batch_size=batch_size, drop_last=drop_last, sampler=finite_sampler)
 
-
-                    if drop_last:
-                        batches_per_epoch = dataset_size // batch_size
-                    else:
-                        batches_per_epoch = (dataset_size + batch_size - 1) // batch_size
-
-                    samples = []
-                    iterator = iter(infinite_loader)
-                    for i in range(n_epochs):
-                        for j in range(batches_per_epoch):
-                            samples.append(next(iterator).clone())
-                    samples = torch.concat(samples)
-                    finite_samples = []
-                    for i in range(n_epochs):
-                        for x in finite_loader:
-                            finite_samples.append(x.clone())
-                    finite_samples = torch.concat(finite_samples)
-                    self.assertEqual(finite_samples.tolist(), samples.tolist())
-                    if not drop_last:
-                        uniques, counts = samples.unique(return_counts=True)
-                        self.assertTrue(torch.all(counts == counts[0]))
-                        self.assertEqual(uniques.tolist(), list(range(dataset_size)))
-                    else:
-                        for i in range(n_epochs):
-                            start = batches_per_epoch * batch_size * i
-                            end = batches_per_epoch * batch_size * (i + 1)
-                            self.assertTrue(len(samples[start:end].unique()) == len(samples[start:end]))
-
+                    infinite_iter = iter(infinite_loader)
+                    for expected_epoch_counter in range(n_epochs):
+                        for expected_batch_counter, expected_batch in enumerate(finite_loader):
+                            actual_epoch_counter, actual_batch_counter, actual_batch = next(infinite_iter)
+                            self.assertEqual(expected_epoch_counter, actual_epoch_counter)
+                            self.assertEqual(expected_batch_counter, actual_batch_counter)
+                            self.assertTrue(torch.all(expected_batch == actual_batch))
