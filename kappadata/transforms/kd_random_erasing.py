20e9bbb738c3a21985b30d2b97c24de0db0d7db9
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-03 22:03:38 +0100

    KDRandomErasing

---
+++ kappadata/transforms/kd_random_erasing.py
@@ -0,0 +1,75 @@
+import numpy as np
+import sys
+import math
+from .base.kd_random_apply_base import KDRandomApplyBase
+import torch
+
+class KDRandomErasing(KDRandomApplyBase):
+    """
+    adaption of timm.data.random_erasing.RandomErasing
+    additional features:
+    - deterministic behavior by setting seed
+    removed features:
+    - support for batchwise apply (intended to be used during dataloading, not during model forward pass)
+    """
+    def __init__(
+            self,
+            min_area=0.02,
+            max_area=1/3,
+            min_aspect=0.3,
+            max_aspect=None,
+            mode="zeros",
+            min_count=1,
+            max_count=None,
+            **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.min_area = min_area
+        self.max_area = max_area
+        max_aspect = max_aspect or 1 / min_aspect
+        self.log_aspect_ratio = (math.log(min_aspect), math.log(max_aspect))
+        self.min_count = min_count
+        self.max_count = max_count or min_count
+        self.mode = mode.lower()
+        if self.mode == "zeros":
+            self._get_replacement = self._get_replacement_zeros
+        elif self.mode == "channelwise":
+            self._get_replacement = self._get_replacement_channelwise
+        elif self.mode == "pixelwise":
+            self._get_replacement = self._get_replacement_pixelwise
+        else:
+            raise NotImplementedError(f"mode '{self.mode}' not supported (use zeros, channelwise or pixelwise)")
+
+    @staticmethod
+    def _get_replacement_zeros(c, h, w):
+        return torch.zeros(c, h, w)
+
+    # noinspection PyUnusedLocal
+    def _get_replacement_channelwise(self, c, h, w):
+        return torch.from_numpy(self.rng.standard_normal(size=(c, 1, 1), dtype=np.float32))
+
+    def _get_replacement_pixelwise(self, c, h, w):
+        return torch.from_numpy(self.rng.standard_normal(size=(c, h, w), dtype=np.float32))
+
+    def forward(self, x, ctx):
+        # sample how many rectangles to erase
+        if self.min_count == self.max_count:
+            n_rects = self.min_count
+        else:
+            n_rects = int(self.rng.integers(self.min_count, self.max_count))
+
+        c, img_h, img_w = x.shape
+        area_per_rect = img_h * img_w / n_rects
+        for _ in range(n_rects):
+            # try 10 times -> skip if not successful
+            for _ in range(10):
+                target_area = self.rng.uniform(self.min_area, self.max_area) * area_per_rect
+                aspect_ratio = math.exp(self.rng.uniform(*self.log_aspect_ratio))
+                h = int(round(math.sqrt(target_area * aspect_ratio)))
+                w = int(round(math.sqrt(target_area / aspect_ratio)))
+                if w < img_w and h < img_h:
+                    top = int(self.rng.integers(0, img_h - h))
+                    left = int(self.rng.integers(0, img_w - w))
+                    x[:, top:top + h, left:left + w] = self._get_replacement(c=c, h=h, w=w)
+                    break
+        return x
---
+++ tests_unit/transforms/test_kd_random_erasing.py
@@ -0,0 +1,35 @@
+import numpy as np
+import unittest
+from unittest.mock import patch
+
+import torch
+# noinspection PyPackageRequirements
+from timm.data.random_erasing import RandomErasing
+from kappadata.transforms.kd_random_erasing import KDRandomErasing
+
+class TestKDRandomErasing(unittest.TestCase):
+    @staticmethod
+    def _run(run_fn):
+        patch_rng = np.random.default_rng(seed=5)
+        patch_normal_fn = lambda self: torch.from_numpy(patch_rng.standard_normal(size=self.shape, dtype=np.float32))
+        with patch("random.random", lambda: patch_rng.random()):
+            with patch("random.uniform", lambda low, high: patch_rng.uniform(low, high)):
+                with patch("random.randint", lambda low, high: int(patch_rng.integers(low, high))):
+                    with patch("torch.Tensor.normal_", patch_normal_fn):
+                        return run_fn()
+
+    @staticmethod
+    def _forward(images, fn):
+        return [fn(img) for img in images]
+
+    def test_equivalent_to_timm(self):
+        images = torch.randn(16, 3, 32, 32, generator=torch.Generator().manual_seed(3))
+        timm_fn = RandomErasing(probability=0.25, mode="pixel", device="cpu")
+        kd_fn = KDRandomErasing(p=0.25, mode="pixelwise")
+
+        timm_images = self._run(lambda: self._forward(images, timm_fn))
+        kd_images = self._forward(images, kd_fn)
+        for i, (timm_image, kd_image) in enumerate(zip(timm_images, kd_images)):
+            self.assertTrue(torch.all(timm_image == kd_image))
+
+
