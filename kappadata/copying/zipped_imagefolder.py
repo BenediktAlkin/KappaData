ade33071818cbe20f2cb35ad830dd390289299d0
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-04 12:45:07 +0100

    job

--- kappadata/copying/zipped_imagefolder.py
+++ kappadata/copying/zipped_imagefolder.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 from pathlib import Path
+import zipfile
+import joblib
 
 def create_zipped_imagefolder_classwise(src, dst):
     src_path = Path(src).expanduser()
@@ -18,15 +20,29 @@ def create_zipped_imagefolder_classwise(src, dst):
             root_dir=src_uri,
         )
 
-def unzip_imagefolder_classwise(src, dst):
+def _unzip(src, dst):
+    with zipfile.ZipFile(src) as f:
+        f.extractall(dst)
+
+def unzip_imagefolder_classwise(src, dst, num_workers=0):
     src_path = Path(src).expanduser()
     assert src_path.exists(), f"src_path '{src_path}' doesn't exist"
     dst_path = Path(dst).expanduser()
     dst_path.mkdir(exist_ok=True, parents=True)
 
+    # compose jobs
+    jobargs = []
     for item in os.listdir(src_path):
         assert item.endswith(".zip")
         dst_uri = (dst_path / item).with_suffix("")
-        with zipfile.ZipFile(src_path / item) as f:
-            f.extractall(dst_uri)
-        break
+        src_uri = src_path / item
+        jobargs.append((src_uri, dst_uri))
+
+    # run jobs
+    if num_workers <= 1:
+        for src, dst in jobargs:
+            _unzip(src, dst)
+    else:
+        jobs = joblib.delayed(_unzip)(src, dst)
+        pool = joblib.Parallel(n_jobs=num_workers)
+        pool(jobs)
--- main_unzip_imagefolder.py
+++ main_unzip_imagefolder.py
@@ -5,10 +5,11 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--src", type=str, required=True)
     parser.add_argument("--dst", type=str, required=True)
+    parser.add_argument("--num_workers", type=int, default=0)
     return vars(parser.parse_args())
 
-def main(src, dst):
-    unzip_imagefolder_classwise(src=src, dst=dst)
+def main(src, dst, num_workers):
+    unzip_imagefolder_classwise(src=src, dst=dst, num_workers=num_workers)
 
 
 if __name__ == "__main__":
