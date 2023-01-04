4d9148501416bcc5e5b3f451340e6da3a74c45ea
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-04 12:48:48 +0100

    Update zipped_imagefolder.py

--- kappadata/copying/zipped_imagefolder.py
+++ kappadata/copying/zipped_imagefolder.py
@@ -3,6 +3,7 @@ import shutil
 from pathlib import Path
 import zipfile
 import joblib
+from time import time
 
 def create_zipped_imagefolder_classwise(src, dst):
     src_path = Path(src).expanduser()
@@ -24,7 +25,7 @@ def _unzip(src, dst):
     with zipfile.ZipFile(src) as f:
         f.extractall(dst)
 
-def unzip_imagefolder_classwise(src, dst, num_workers=0):
+def unzip_imagefolder_classwise(src, dst, num_workers=0, log_fn=print):
     src_path = Path(src).expanduser()
     assert src_path.exists(), f"src_path '{src_path}' doesn't exist"
     dst_path = Path(dst).expanduser()
@@ -39,10 +40,13 @@ def unzip_imagefolder_classwise(src, dst, num_workers=0):
         jobargs.append((src_uri, dst_uri))
 
     # run jobs
+    start_time = time()
     if num_workers <= 1:
         for src, dst in jobargs:
             _unzip(src, dst)
     else:
-        jobs = joblib.delayed(_unzip)(src, dst)
+        jobs = [joblib.delayed(_unzip)(src, dst) for src, dst in jobargs]
         pool = joblib.Parallel(n_jobs=num_workers)
         pool(jobs)
+    end_time = time()
+    log_fn(f"unzipping {len(jobargs)} from {src_path} to {dst_path} took {end_time - start_time:.2f}s")
