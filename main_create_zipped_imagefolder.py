e2ea45d5f1c98d067a9d2b4ffaec37baa43a3254
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-04 10:33:00 +0100

    Create main_create_zipped_imagefolder.py

---
+++ main_create_zipped_imagefolder.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from kappadata.copying.create_zipped_imagefolder import create_zipped_imagefolder_classwise
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--src", type=str, required=True)
+    parser.add_argument("--dst", type=str, required=True)
+    return vars(parser.parse_args())
+
+def main(src, dst):
+    create_zipped_imagefolder_classwise(src=src, dst=dst)
+
+
+if __name__ == "__main__":
+    main(**parse_args())
