580b0ba8375b0db480f9828ec414e2bc6417a2ce
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-04 02:01:25 +0100

    Update __init__.py

--- kappadata/__init__.py
+++ kappadata/__init__.py
@@ -28,7 +28,6 @@ from .transforms.kd_random_erasing import KDRandomErasing
 from .transforms.kd_random_gaussian_blur_pil import KDRandomGaussianBlurPIL
 from .transforms.kd_random_gaussian_blur_tv import KDRandomGaussianBlurTV
 from .transforms.kd_random_grayscale import KDRandomGrayscale
-from .transforms.kd_random_grayscale import KDRandomGrayscale
 from .transforms.kd_random_horizontal_flip import KDRandomHorizontalFlip
 from .transforms.kd_random_resized_crop import KDRandomResizedCrop
 from .transforms.kd_random_solarize import KDRandomSolarize
