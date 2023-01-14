3764864c3b99bcc0cfe6b2361cac2ea39a226adc
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-14 20:39:44 +0100

    fix

--- kappadata/collators/__init__.py
+++ kappadata/collators/__init__.py
@@ -1,2 +1,5 @@
+# base
+from .base.kd_compose_collator import KDComposeCollator
+#
 from .kd_mix_collator import KDMixCollator
 from .pad_sequences_collator import PadSequencesCollator
--- kappadata/common/collators/mae_finetune_mix_collator.py
+++ kappadata/common/collators/mae_finetune_mix_collator.py
@@ -1,8 +1,8 @@
 import kappadata.collators as kdc
 
-class MAEFinetuneMixCollator(kdc.KDMixCollator):
+class MAEFinetuneMixCollator(kdc.KDComposeCollator):
     def __init__(self):
-        super().__init__(
+        mix_collator = kdc.KDMixCollator(
             mixup_alpha=0.8,
             cutmix_alpha=1.0,
             mixup_p=0.5,
@@ -10,4 +10,8 @@ class MAEFinetuneMixCollator(kdc.KDMixCollator):
             apply_mode="batch",
             lamb_mode="batch",
             shuffle_mode="flip",
+        )
+        super().__init__(
+            collators=[mix_collator],
+            dataset_mode="x class",
         )
