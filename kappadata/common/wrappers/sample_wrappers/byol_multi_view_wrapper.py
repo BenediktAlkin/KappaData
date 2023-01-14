25827a1ccee8ffaafa7616a7dd67b49bb80fce62
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-14 22:45:27 +0100

    Update byol_multi_view_wrapper.py

--- kappadata/common/wrappers/sample_wrappers/byol_multi_view_wrapper.py
+++ kappadata/common/wrappers/sample_wrappers/byol_multi_view_wrapper.py
@@ -2,5 +2,5 @@ from kappadata.wrappers.sample_wrappers.multi_view_wrapper import MultiViewWrapp
 from kappadata.common.transforms.byol_transforms import BYOLTransform0, BYOLTransform1
 
 class BYOLMultiViewWrapper(MultiViewWrapper):
-    def __init__(self):
-        super().__init__(configs=[BYOLTransform0(), BYOLTransform1()])
+    def __init__(self, dataset):
+        super().__init__(dataset=dataset, configs=[BYOLTransform0(), BYOLTransform1()])
