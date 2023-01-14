2566a5877fc64a512ae41eed32026d64a8aca55a
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-14 17:15:09 +0100

    Update __init__.py

--- kappadata/transforms/__init__.py
+++ kappadata/transforms/__init__.py
@@ -1,14 +1,33 @@
 # base
 from .base.kd_compose_transform import KDComposeTransform
 from .base.kd_transform import KDTransform
-# augs
-from .image_pos_embed_grid import ImagePosEmbedGrid
-from .image_pos_embed_sincos import ImagePosEmbedSincos
 # norm
 from .norm.kd_cifar10_norm import KDCifar10Norm
 from .norm.kd_image_net_norm import KDImageNetNorm
 from .norm.kd_image_norm import KDImageNorm
 from .norm.kd_image_range_norm import KDImageRangeNorm
+# augs
+from .image_pos_embed_grid import ImagePosEmbedGrid
+from .image_pos_embed_sincos import ImagePosEmbedSincos
+#
+from .kd_color_jitter import KDColorJitter
+from .kd_gaussian_blur_tv import KDGaussianBlurTV
+from .kd_gaussian_blur_pil import KDGaussianBlurPIL
+from .kd_rand_augment import KDRandAugment
+from .kd_rand_augment_custom import KDRandAugmentCustom
+from .kd_random_apply import KDRandomApply
+from .kd_random_color_jitter import KDRandomColorJitter
+from .kd_random_crop import KDRandomCrop
+from .kd_random_erasing import KDRandomErasing
+from .kd_random_gaussian_blur_pil import KDRandomGaussianBlurPIL
+from .kd_random_gaussian_blur_tv import KDRandomGaussianBlurTV
+from .kd_random_grayscale import KDRandomGrayscale
+from .kd_random_horizontal_flip import KDRandomHorizontalFlip
+from .kd_random_resized_crop import KDRandomResizedCrop
+from .kd_random_solarize import KDRandomSolarize
+#
 from .patchify_image import PatchifyImage
 from .patchwise_random_rotation import PatchwiseRandomRotation
+from .patchwise_shuffle import PatchwiseShuffle
+from .save_state_to_context_transform import SaveStateToContextTransform
 from .unpatchify_image import UnpatchifyImage
