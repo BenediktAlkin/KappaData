fc9b10889985c9eec2f72f9f73f250feba292e9d
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-19 17:45:16 +0100

    Update main_create_zipped_imagefolder.py

--- main_create_zipped_imagefolder.py
+++ main_create_zipped_imagefolder.py
@@ -1,3 +1,4 @@
+import shutil
 from argparse import ArgumentParser
 
 from kappadata.copying.image_folder import create_zipped_imagefolder_classwise
@@ -7,11 +8,18 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--src", type=str, required=True)
     parser.add_argument("--dst", type=str, required=True)
+    classwise_group = parser.add_mutually_exclusive_group()
+    classwise_group.add_argument("--splitwise", action="store_false", dest="classwise")
+    classwise_group.add_argument("--classwise", action="store_true")
+    classwise_group.set_defaults(classwise=True)
     return vars(parser.parse_args())
 
 
-def main(src, dst):
-    create_zipped_imagefolder_classwise(src=src, dst=dst)
+def main(src, dst, classwise):
+    if classwise:
+        shutil.make_archive(src, "zip", dst)
+    else:
+        create_zipped_imagefolder_classwise(src=src, dst=dst)
 
 
 if __name__ == "__main__":
