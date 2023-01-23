1a9445e54bf1e00bb1ee29d2d71a1a05a60b0d1e
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-01-24 00:20:03 +0100

    multicrop

---
+++ tests_util/modules/memorize_shape_module.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+class MemorizeShapeModule(nn.Module):
+    def __init__(self, module):
+        super().__init__()
+        self.module = module
+        self.shapes = []
+
+    def forward(self, x):
+        self.shapes.append(tuple(x.shape))
+        return self.module(x)
