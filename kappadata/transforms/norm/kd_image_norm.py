ecbfd841de3ce9cd1b3c81cf3b7845e90d89cf28
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-02-17 13:21:47 +0100

    draft

--- kappadata/transforms/norm/kd_image_norm.py
+++ kappadata/transforms/norm/kd_image_norm.py
@@ -1,3 +1,4 @@
+import einops
 import torch
 from torchvision.transforms.functional import to_tensor, normalize
 
@@ -14,14 +15,33 @@ class KDImageNorm(KDNormBase):
     def normalize(self, x, inplace=True):
         if not torch.is_tensor(x):
             x = to_tensor(x)
-        return normalize(x, mean=self.mean, std=self.std, inplace=inplace)
+        if x.ndim == 3:
+            # per sample norm
+            return normalize(x, mean=self.mean, std=self.std, inplace=inplace)
+        else:
+            # per batch norm
+            assert x.ndim == 4
+            mean = einops.rearrange(torch.tensor(self.mean, device=x.device), "channels -> 1 channels 1 1")
+            std = einops.rearrange(torch.tensor(self.std, device=x.device), "channels -> 1 channels 1 1")
+            if inplace:
+                return x.sub_(mean).div_(std)
+            return (x - mean) / std
 
     def denormalize(self, x, inplace=True):
         if not torch.is_tensor(x):
             x = to_tensor(x)
-        inv_std = tuple(1. / std for std in self.std)
-        inv_mean = tuple(-mean for mean in self.mean)
-        zero = tuple(0. for _ in self.mean)
-        one = tuple(1. for _ in self.std)
-        x = normalize(x, mean=zero, std=inv_std, inplace=inplace)
-        return normalize(x, mean=inv_mean, std=one, inplace=inplace)
+        if x.ndim == 3:
+            # per sample norm
+            inv_std = tuple(1. / std for std in self.std)
+            inv_mean = tuple(-mean for mean in self.mean)
+            zero = tuple(0. for _ in self.mean)
+            one = tuple(1. for _ in self.std)
+            x = normalize(x, mean=zero, std=inv_std, inplace=inplace)
+            return normalize(x, mean=inv_mean, std=one, inplace=inplace)
+        else:
+            # per batch norm
+            assert x.ndim == 4
+            mean = einops.rearrange(torch.tensor(self.mean, device=x.device), "channels -> 1 channels 1 1")
+            std = einops.rearrange(torch.tensor(self.std, device=x.device), "channels -> 1 channels 1 1")
+            if inplace:
+                return x.mul_(std).add_(mean)
--- kappadata/transforms/norm/kd_image_range_norm.py
+++ kappadata/transforms/norm/kd_image_range_norm.py
@@ -8,17 +8,33 @@ class KDImageRangeNorm(KDNormBase):
     def normalize(self, x, inplace=True):
         if not torch.is_tensor(x):
             x = to_tensor(x)
-        n_channels = x.size(0)
-        values = tuple(.5 for _ in range(n_channels))
-        return normalize(x, mean=values, std=values, inplace=inplace)
+        if x.ndim == 3:
+            # per sample norm
+            n_channels = x.size(0)
+            values = tuple(.5 for _ in range(n_channels))
+            return normalize(x, mean=values, std=values, inplace=inplace)
+        else:
+            # per batch norm
+            assert x.ndim == 4
+            if inplace:
+                return x.mul_(2).sub_(1)
+            return x * 2 - 1
 
     def denormalize(self, x, inplace=True):
         if not torch.is_tensor(x):
             x = to_tensor(x)
-        n_channels = x.size(0)
-        std = tuple(2. for _ in range(n_channels))
-        mean = tuple(-.5 for _ in range(n_channels))
-        zero = tuple(0. for _ in range(n_channels))
-        one = tuple(1. for _ in range(n_channels))
-        x = normalize(x, mean=zero, std=std, inplace=inplace)
-        return normalize(x, mean=mean, std=one, inplace=inplace)
+        if x.ndim == 3:
+            # per sample norm
+            n_channels = x.size(0)
+            std = tuple(2. for _ in range(n_channels))
+            mean = tuple(-.5 for _ in range(n_channels))
+            zero = tuple(0. for _ in range(n_channels))
+            one = tuple(1. for _ in range(n_channels))
+            x = normalize(x, mean=zero, std=std, inplace=inplace)
+            return normalize(x, mean=mean, std=one, inplace=inplace)
+        else:
+            # per batch norm
+            assert x.ndim == 4
+            if inplace:
+                return x.add_(1).div_(2)
+            return (x + 1) / 2
