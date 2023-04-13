1347f326c76d830a6d9eac514ea826e5f4230a2b
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-04-13 14:33:18 +0200

    visualize_transform

---
+++ kappadata/visualization/visualize_transform.py
@@ -0,0 +1,27 @@
+import torch
+import einops
+from torchvision.transforms.functional import to_tensor, resize, to_pil_image, InterpolationMode
+from kappadata.transforms.patchify_image import PatchifyImage
+from kappadata.transforms.unpatchify_image import UnpatchifyImage
+from kappadata.utils.param_checking import to_2tuple
+import numpy as np
+from kappadata.transforms.base.kd_stochastic_transform import KDStochasticTransform
+from kappadata.transforms.base.kd_compose_transform import KDComposeTransform
+from PIL import Image
+
+def visualize_transform(img, transform, size=300, seed=0):
+    assert isinstance(img, Image.Image) or (torch.is_tensor(img) and img.ndim == 3)
+
+    # preprocess
+    img = resize(img, size=to_2tuple(size), interpolation=InterpolationMode.BILINEAR)
+
+    # apply transform
+    rng = np.random.default_rng(seed=seed)
+    if isinstance(transform, (KDStochasticTransform, KDComposeTransform)):
+        transform.set_rng(rng)
+    img = transform(img)
+
+    # to image
+    if torch.is_tensor(img):
+        img = to_pil_image(img)
+    return img
---
+++ main_visualize_transform.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import torch
+from kappadata.visualization.visualize_transform import visualize_transform
+from pathlib import Path
+from torchvision.datasets.folder import default_loader
+from argparse import ArgumentParser
+from kappadata.transforms.base.kd_compose_transform import KDComposeTransform
+from kappadata.transforms.kd_gaussian_blur_pil import KDGaussianBlurPIL
+from kappadata.transforms.kd_random_color_jitter import KDRandomColorJitter
+from kappadata.transforms.kd_random_gaussian_blur_pil import KDRandomGaussianBlurPIL
+from kappadata.transforms.kd_random_grayscale import KDRandomGrayscale
+from kappadata.transforms.kd_random_horizontal_flip import KDRandomHorizontalFlip
+from kappadata.transforms.kd_random_resized_crop import KDRandomResizedCrop
+from kappadata.transforms.kd_random_solarize import KDRandomSolarize
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--root", required=True, type=str)
+    parser.add_argument("--repeat", default=100, type=int)
+    return vars(parser.parse_args())
+
+
+def main(root, repeat):
+    # load image
+    root = Path(root).expanduser()
+    img = default_loader(root)
+
+    size = 300
+    transform = KDComposeTransform([
+        KDRandomHorizontalFlip(),
+        KDRandomColorJitter(p=0.8, brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1),
+        KDGaussianBlurPIL(sigma=(0.1, 2.0)),
+        KDRandomGrayscale(p=0.2),
+    ])
+    # transform = KDComposeTransform([
+    #     KDRandomHorizontalFlip(),
+    #     KDRandomColorJitter(p=0.8, brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1),
+    #     KDRandomGaussianBlurPIL(p=0.1, sigma=(0.1, 2.0)),
+    #     KDRandomGrayscale(p=0.2),
+    #     KDRandomSolarize(p=0.2, threshold=128),
+    # ])
+
+    temp_dir = Path("temp/Transform")
+    temp_dir.mkdir(exist_ok=True, parents=True)
+    for i in range(repeat):
+        # visualize
+        transformed = visualize_transform(img, transform, size=size, seed=i)
+        transformed.save(temp_dir / f"{root.name}_{i}.png")
+
+
+if __name__ == "__main__":
+    main(**parse_args())
