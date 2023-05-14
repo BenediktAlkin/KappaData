8be9906924d7e2d13f130d3a5a2c123900e8d551
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-05-14 22:07:13 +0200

    tests

--- kappadata/transforms/semseg/kd_semseg_random_resize.py
+++ kappadata/transforms/semseg/kd_semseg_random_resize.py
@@ -20,12 +20,12 @@ class KDSemsegRandomResize(KDStochasticTransform):
         new_size = self.get_params()
         x, semseg = xsemseg
         x = resize(x, new_size, self.interpolation)
-        semseg = resize(semseg, new_size, "nearest")
+        semseg = resize(semseg, new_size, InterpolationMode.NEAREST)
         return x, semseg
 
     def get_params(self):
         ratio = self.rng.uniform(self.ratio[0], self.ratio[1])
         height, width = self.base_size
-        new_height = height * ratio
-        new_width = width * ratio
+        new_height = round(height * ratio)
+        new_width = round(width * ratio)
         return new_height, new_width
