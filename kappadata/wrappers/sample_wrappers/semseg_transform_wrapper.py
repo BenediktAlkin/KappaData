eda48bc05aa7faf2768834ae8de2d8f2783a6663
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-05-14 17:04:15 +0200

    Update semseg_transform_wrapper.py

--- kappadata/wrappers/sample_wrappers/semseg_transform_wrapper.py
+++ kappadata/wrappers/sample_wrappers/semseg_transform_wrapper.py
@@ -5,17 +5,29 @@ from kappadata.factory import object_to_transform
 
 
 class SemsegTransformWrapper(KDWrapper):
-    def __init__(self, dataset, shared_transform, x_transform=None, seed=None):
+    def __init__(
+            self,
+            dataset,
+            x_pre_transform=None,
+            semseg_pre_transform=None,
+            shared_transform=None,
+            x_post_transform=None,
+            semseg_post_transform=None,
+            seed=None,
+    ):
         super().__init__(dataset=dataset)
+        self.x_pre_transform = object_to_transform(x_pre_transform)
+        self.semseg_pre_transform = object_to_transform(semseg_pre_transform)
         self.shared_transform = object_to_transform(shared_transform)
-        self.x_transform = object_to_transform(x_transform)
+        self.x_post_transform = object_to_transform(x_post_transform)
+        self.semseg_post_transform = object_to_transform(semseg_post_transform)
         self.seed = seed
 
     @property
     def requires_propagate_ctx(self):
         return True
 
-    def _shared_transform(self, item, idx, ctx):
+    def _shared_transform(self, item, pre_transform, idx, ctx):
         # generate seed or retrieve seed from ctx
         assert ctx is not None
         if "semseg_seed" not in ctx:
@@ -24,25 +36,37 @@ class SemsegTransformWrapper(KDWrapper):
             ctx["semseg_seed"] = seed
         else:
             seed = ctx["semseg_seed"]
-        # apply shared_transform
         rng = np.random.default_rng(seed=seed + idx)
-        assert self.shared_transform.is_kd_transform
-        self.shared_transform.set_rng(rng)
-        item = self.shared_transform(item, ctx=ctx)
+        # apply pre_transform (shared rng)
+        if pre_transform is not None:
+            assert pre_transform.is_kd_transform
+            pre_transform.set_rng(rng)
+            item = pre_transform(item, ctx=ctx)
+        # apply shared_transform (shared rng)
+        if self.shared_transform is not None:
+            assert self.shared_transform.is_kd_transform
+            self.shared_transform.set_rng(rng)
+            item = self.shared_transform(item, ctx=ctx)
         return item
 
     def getitem_x(self, idx, ctx=None):
         x = self.dataset.getitem_x(idx, ctx=ctx)
-        x = self._shared_transform(item=x, idx=idx, ctx=ctx)
-        # apply x_transform
-        if self.x_transform is not None:
+        x = self._shared_transform(item=x, pre_transform=self.x_pre_transform, idx=idx, ctx=ctx)
+        # apply x_post_transform (independent rng)
+        if self.x_post_transform is not None:
             if self.seed is not None:
-                x_transform_rng = np.random.default_rng(seed=self.seed + idx)
-                self.x_transform.set_rng(x_transform_rng)
-            x = self.x_transform(x, ctx=ctx)
+                x_post_transform_rng = np.random.default_rng(seed=self.seed + idx)
+                self.x_post_transform.set_rng(x_post_transform_rng)
+            x = self.x_post_transform(x, ctx=ctx)
         return x
 
     def getitem_semseg(self, idx, ctx=None):
         semseg = self.dataset.getitem_semseg(idx, ctx=ctx)
-        semseg = self._shared_transform(item=semseg, idx=idx, ctx=ctx)
+        semseg = self._shared_transform(item=semseg, pre_transform=self.semseg_pre_transform, idx=idx, ctx=ctx)
+        # apply semseg_post_transform (independent rng)
+        if self.semseg_post_transform is not None:
+            if self.seed is not None:
+                semseg_post_transform_rng = np.random.default_rng(seed=self.seed + idx)
+                self.semseg_post_transform.set_rng(semseg_post_transform_rng)
+            semseg = self.semseg_post_transform(semseg, ctx=ctx)
         return semseg
