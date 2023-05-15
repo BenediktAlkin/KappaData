ae04a667fc2f72b2a784c708d443609673009c6c
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-05-15 09:59:23 +0200

    KDSemsegResize

---
+++ kappadata/transforms/semseg/kd_semseg_resize.py
@@ -0,0 +1,28 @@
+import math
+
+import numpy as np
+import torch
+from torchvision.transforms import InterpolationMode
+from torchvision.transforms.functional import resize, get_image_size
+
+from kappadata.utils.param_checking import to_2tuple
+from kappadata.transforms.base.kd_transform import KDTransform
+
+
+class KDSemsegResize(KDTransform):
+    def __init__(self, size, interpolation="bilinear", **kwargs):
+        super().__init__(**kwargs)
+        self.size = to_2tuple(size)
+        self.interpolation = InterpolationMode(interpolation)
+
+    def __call__(self, xsemseg, ctx=None):
+        x, semseg = xsemseg
+        x = resize(x, self.size, self.interpolation)
+        squeeze_semseg = False
+        if torch.is_tensor(semseg):
+            semseg = semseg.unsqueeze(0)
+            squeeze_semseg = True
+        semseg = resize(semseg, self.size, InterpolationMode.NEAREST)
+        if squeeze_semseg:
+            semseg = semseg.squeeze(0)
+        return x, semseg
---
+++ tests_unit/transforms/semseg/test_kd_semseg_resize.py
@@ -0,0 +1,21 @@
+import unittest
+
+import einops
+import numpy as np
+import torch
+from torchvision.transforms.functional import to_pil_image, to_tensor
+
+from kappadata.transforms.semseg.kd_semseg_resize import KDSemsegResize
+
+
+class TestKDSemsegResize(unittest.TestCase):
+    def test_same_op(self):
+        rng = torch.Generator().manual_seed(98435)
+        semseg = torch.randint(150, size=(16, 16), generator=rng)
+        x = einops.rearrange(semseg / 255, "h w -> 1 h w")
+        semseg = semseg.unsqueeze(0)
+
+        t = KDSemsegResize(size=(64, 32), interpolation="nearest")
+        t.set_rng(np.random.default_rng(seed=9823))
+        x_t, semseg_t = t((x, semseg))
+        self.assertTrue(torch.all(x_t * 255 == semseg_t))
