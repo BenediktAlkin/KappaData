7d63e62ff55372795f2f81355a86356294e48d3e
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-06-20 13:44:33 +0200

    KDHorizontalFlip

---
+++ kappadata/transforms/kd_horizontal_flip.py
@@ -0,0 +1,9 @@
+import torch
+from torchvision.transforms.functional import hflip
+
+from .base.kd_transform import KDTransform
+
+
+class KDHorizontalFlip(KDTransform):
+    def __call__(self, x, ctx=None):
+        return hflip(x)
