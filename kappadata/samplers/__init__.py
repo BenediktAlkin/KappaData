8ce8c3aec074d1014695352bf8777b5e8e2361be
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-06-27 23:34:47 +0200

    Update __init__.py

--- kappadata/samplers/__init__.py
+++ kappadata/samplers/__init__.py
@@ -3,3 +3,4 @@ from .infinite_batch_sampler import InfiniteBatchSampler
 from .interleaved_sampler import InterleavedSampler, InterleavedSamplerConfig
 from .random_sampler import RandomSampler
 from .semi_sampler import SemiSampler
+from .sequential_sampler import SequentialSampler
