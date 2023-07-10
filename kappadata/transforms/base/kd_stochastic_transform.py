62e66057e19f0e001a09c2148787f3f980cc1cdd
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-07-10 22:29:33 +0200

    fix

--- kappadata/transforms/base/kd_stochastic_transform.py
+++ kappadata/transforms/base/kd_stochastic_transform.py
@@ -6,27 +6,20 @@ from .kd_transform import KDTransform
 class KDStochasticTransform(KDTransform):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._rng = None
-
-    @property
-    def rng(self):
-        # if rng is initialized in __init__ all worker processes start from the same rng
-        # by lazily initializing self._rng is None when the process is spawned so we can control the initial state
-        if self._rng is None:
-            # problem: np.random.default_rng() is not influenced by the numpy global seed
-            # info: dataloader implicitly sets a different global seed per worker process
-            # solution: seed with a random int generated from the global rng
-            # result: transform will be deterministic as long as num_workers are the same
-            seed = np.random.randint(np.iinfo(np.int32).max)
-            self._rng =  np.random.default_rng(seed=seed)
-        return self._rng
+        # np.random.default_rng() is not dependent on the global numpy seed -> sample seed from np.random.randint
+        # ATTENTION: as the rng is initialized in the constructor it will be the same on all dataloader processes
+        # -> overwrite in worker_init_fn to have different seeds per process
+        # -> determinism is dependent on num_workers
+        # TODO add a check
+        seed = np.random.randint(np.iinfo(np.int32).max)
+        self.rng = np.random.default_rng(seed=seed)
 
     @property
     def is_deterministic(self):
         return False
 
     def set_rng(self, rng):
-        self._rng = rng
+        self.rng = rng
         return self
 
     def __call__(self, x, ctx=None):
