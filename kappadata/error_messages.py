e943a5f69a53d3e9198b15787eca5963dcd0347c
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-07-10 16:02:17 +0200

    remove mix_wrapper

--- kappadata/error_messages.py
+++ kappadata/error_messages.py
@@ -7,9 +7,5 @@ def too_little_samples_for_class(class_idx, actual, expected):
     return f"class {class_idx} has only {actual} samples (requires at least {expected} samples)"
 
 
-KD_MIX_WRAPPER_REQUIRES_SEED_OR_CONTEXT = (
-    "KDMixWrapper requires either a context or a seed to ensure same mixing of samples and labels "
-    "(NOTE: using a seed will additionally make the transform deterministc per sample)"
-)
 
 REQUIRES_MIXUP_P_OR_CUTMIX_P = "at least one of mixup_p or cutmix_p is required"
--- kappadata/wrappers/sample_wrappers/__init__.py
+++ kappadata/wrappers/sample_wrappers/__init__.py
@@ -1,4 +1,3 @@
-from .kd_mix_wrapper import KDMixWrapper
 from .kd_multi_view_wrapper import KDMultiViewWrapper
 from .label_smoothing_wrapper import LabelSmoothingWrapper
 from .one_hot_wrapper import OneHotWrapper
