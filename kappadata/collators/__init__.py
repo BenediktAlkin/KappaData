8d6fb052e7c3820c6a5cf3c7b835cd5ef6d1b047
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-07-26 17:08:34 +0200

    Update __init__.py

--- kappadata/collators/__init__.py
+++ kappadata/collators/__init__.py
@@ -1,3 +1,5 @@
 from .base import *
+from .kd_dino_mask_collator import KDDinoMaskCollator
+from .kd_ijepa_mask_collator import KDIjepaMaskCollator
 from .kd_mix_collator import KDMixCollator
 from .pad_sequences_collator import PadSequencesCollator
