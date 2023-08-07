ab5690ba6a6985d7793610111dcb441414cd7af6
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-08-08 01:46:36 +0200

    Update x_transform_wrapper.py

--- kappadata/wrappers/sample_wrappers/x_transform_wrapper.py
+++ kappadata/wrappers/sample_wrappers/x_transform_wrapper.py
@@ -6,6 +6,12 @@ class XTransformWrapper(TransformWrapperBase):
         item = self.dataset.getitem_x(idx, ctx=ctx)
         return self._getitem(item=item, idx=idx, ctx=ctx)
 
+    def getitem_class(self, idx, ctx=None):
+        # TODO ugly solution to circumvent XTransformWrapper being skipped when a dataset with a fused operation
+        #  is wrapperd (e.g. XTransformWrapper(KDMixWrapper(dataset)) -> this method is only here because
+        #  operations of a fused operation are checked to be available in the wrapper type
+        return self.dataset.getitem_class(idx, ctx=ctx)
+
     def getitem_xclass(self, idx, ctx=None):
         # TODO ugly solution to circumvent XTransformWrapper being skipped when a dataset with a fused operation
         #  is wrapperd (e.g. XTransformWrapper(KDMixWrapper(dataset))
