96375dcd6f8e4a0d48160f077058e0279a077c95
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-08-10 00:02:57 +0200

    Update __init__.py

--- kappadata/common/transforms/norm/__init__.py
+++ kappadata/common/transforms/norm/__init__.py
@@ -2,7 +2,7 @@ from .kd_ade20k_norm import KDAde20kNorm
 from .kd_audioset_norm import KDAudioSetNorm
 from .kd_cifar100_norm import KDCifar100Norm
 from .kd_cifar10_norm import KDCifar10Norm
-from .kd_esc50_norm import KDEscNorm
+from .kd_esc50_norm import KDEsc50Norm
 from .kd_image_net_norm import KDImageNetNorm
 from .kd_sid_norm import KDSidNorm
 from .kd_spc1_norm import KDSpc1Norm
