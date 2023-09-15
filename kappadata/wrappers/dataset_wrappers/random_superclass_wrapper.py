a1c397fcad565352bbf7507dbe7ca535069eb164
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-09-15 16:13:40 +0200

    superclass_splits

--- kappadata/wrappers/dataset_wrappers/random_superclass_wrapper.py
+++ kappadata/wrappers/dataset_wrappers/random_superclass_wrapper.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import math
 
 import numpy as np
@@ -13,9 +14,11 @@ class RandomSuperclassWrapper(KDWrapper):
     Example output: dataset with 5 classes where each class consists of 2 input classes
     """
 
-    def __init__(self, dataset, classes_per_superclass, seed=None):
+    def __init__(self, dataset, classes_per_superclass, superclass_splits=1, seed=None):
         super().__init__(dataset=dataset)
-        self.classes_per_subclass = classes_per_superclass
+        self.classes_per_superclass = classes_per_superclass
+        self.superclass_splits = superclass_splits
+        self.og_num_classes = math.ceil(dataset.getdim_class() / classes_per_superclass)
         if seed is not None:
             # static pseudo labels (they dont change from epoch to epoch)
             rng = np.random.default_rng(seed=seed)
@@ -23,17 +26,38 @@ class RandomSuperclassWrapper(KDWrapper):
             # dynamic pseudo labels (resampled for every epoch)
             rng = GlobalRng()
         self.perm = rng.permutation(dataset.getdim_class())
+        if superclass_splits > 1:
+            classes = dataset.getall_class()
+            if not isinstance(classes, np.ndarray):
+                classes = np.array(classes)
+            # shuffle classes to avoid patterns
+            perm = rng.permutation(len(classes))
+            classes = classes[perm]
+            # mapping for each sample to its split
+            self.idx_within_class = []
+            counter = defaultdict(int)
+            for cls in classes:
+                self.idx_within_class.append(counter[cls])
+                counter[cls] += 1
+            # invert shuffling
+            inv_perm = np.argsort(perm)
+            self.idx_within_class = np.array(self.idx_within_class)[inv_perm]
+        else:
+            self.idx_within_class = None
 
     def getshape_class(self):
-        return math.ceil(self.dataset.getdim_class() / self.classes_per_subclass),
+        return self.og_num_classes * self.superclass_splits,
 
-    def _map_cls(self, cls):
-        return self.perm[cls] // self.classes_per_subclass
+    def _map_cls(self, idx, cls):
+        cls = self.perm[cls] // self.classes_per_superclass
+        if self.idx_within_class is not None:
+            cls += self.idx_within_class[idx] % self.superclass_splits * self.og_num_classes
+        return cls
 
     def getitem_class(self, idx, ctx=None):
         cls = self.dataset.getitem_class(idx, ctx=ctx)
-        return self._map_cls(cls)
+        return self._map_cls(idx=idx, cls=cls)
 
     def getall_class(self):
         classes = self.dataset.getall_class()
-        return [self._map_cls(cls) for cls in classes]
+        return [self._map_cls(idx=idx, cls=cls) for idx, cls in enumerate(classes)]
--- tests_unit/wrappers/dataset_wrappers/test_random_superclass_wrapper.py
+++ tests_unit/wrappers/dataset_wrappers/test_random_superclass_wrapper.py
@@ -5,7 +5,7 @@ from tests_util.datasets.class_dataset import ClassDataset
 
 
 class TestRandomSuperclassWrapper(unittest.TestCase):
-    def test_simple(self):
+    def test_2superclasses(self):
         ds = RandomSuperclassWrapper(
             dataset=ClassDataset(classes=[0, 1, 2, 3, 0, 1, 2, 3]),
             classes_per_superclass=2,
@@ -16,6 +16,19 @@ class TestRandomSuperclassWrapper(unittest.TestCase):
         self.assertEqual(expected, ds.getall_class())
         self.assertEqual(2, ds.getshape_class()[0])
         self.assertEqual(2, ds.getdim_class())
+
+    def test_2superclasses_2splits(self):
+        ds = RandomSuperclassWrapper(
+            dataset=ClassDataset(classes=[0, 1, 2, 3, 0, 1, 2, 3]),
+            classes_per_superclass=2,
+            superclass_splits=2,
+            seed=2,
+        )
+        expected = [1, 1, 0, 2, 3, 3, 2, 0]
+        self.assertEqual(expected, [ds.getitem_class(i) for i in range(len(ds))])
+        self.assertEqual(expected, ds.getall_class())
+        self.assertEqual(4, ds.getshape_class()[0])
+        self.assertEqual(4, ds.getdim_class())
 
     def test_undivisible_num_class(self):
         ds = RandomSuperclassWrapper(
