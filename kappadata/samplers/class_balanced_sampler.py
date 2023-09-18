5ff91275fcff3f268b31b59083762334e1e70ff1
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-09-18 12:33:39 +0200

    ClassBalancedSampler

---
+++ kappadata/samplers/class_balanced_sampler.py
@@ -0,0 +1,59 @@
+import torch
+
+from kappadata.utils.distributed import get_rank, get_world_size
+from kappadata.utils.getall_class_as_tensor import getall_class_as_tensor
+
+
+class ClassBalancedSampler:
+    def __init__(self, dataset, shuffle=True, samples_per_class=None, seed=0, rank=None, world_size=None):
+        super().__init__()
+        self.dataset = dataset
+        self.shuffle = shuffle
+        self.seed = seed
+        self.rank = rank or get_rank()
+        self.world_size = world_size or get_world_size()
+        self.epoch = 0
+
+        # load/check all classes
+        self.num_classes = max(2, dataset.getdim_class())
+        classes = torch.tensor(dataset.getall_class())
+        unique, counts = classes.unique(return_counts=True)
+        assert classes.ndim == 1
+        assert len(unique) == self.num_classes
+
+        # calculate indices per class
+        self.indices_per_class = [(classes == i).nonzero().squeeze(1) for i in range(self.num_classes)]
+        self.samples_per_class = samples_per_class or counts.max().item()
+
+    @property
+    def effective_length(self):
+        return self.num_classes * self.samples_per_class
+
+    def __len__(self):
+        # adjust to length-per-device and cutoff trailing samples for distributed
+        return self.effective_length // self.world_size
+
+    def set_epoch(self, epoch):
+        self.epoch = epoch
+
+    def __iter__(self):
+        # draw indices for current epoch (same for all ranks)
+        generator = torch.Generator().manual_seed(self.seed + self.epoch)
+        indices = []
+        for indices_per_class in self.indices_per_class:
+            remaining_indices = self.samples_per_class
+            while remaining_indices > 0:
+                if self.shuffle:
+                    perm = torch.randperm(len(indices_per_class), generator=generator)[:remaining_indices]
+                else:
+                    perm = torch.arange(len(indices_per_class))
+                indices.append(indices_per_class[perm])
+                remaining_indices -= len(perm)
+        indices = torch.concat(indices)
+        if self.shuffle:
+            indices = indices[torch.randperm(len(indices), generator=generator)]
+        # distribute among ranks
+        indices = indices[self.rank:self.effective_length:self.world_size].tolist()
+        # drop last
+        indices = indices[:len(self)]
+        yield from indices
---
+++ tests_unit/samplers/test_class_balanced_sampler.py
@@ -0,0 +1,36 @@
+import unittest
+
+import torch
+
+from kappadata.samplers.class_balanced_sampler import ClassBalancedSampler
+from tests_util.datasets import ClassDataset
+
+
+class TestClassBalancedSampler(unittest.TestCase):
+    def test_autolength(self):
+        ds = ClassDataset(classes=[0, 1, 0, 0])
+        sampler = ClassBalancedSampler(ds)
+        self.assertEqual(6, len(sampler))
+
+    def test_manuallength(self):
+        ds = ClassDataset(classes=[0, 1, 0, 0])
+        sampler = ClassBalancedSampler(ds, samples_per_class=2)
+        self.assertEqual(4, len(sampler))
+
+    def test_sample_noshuffle(self):
+        ds = ClassDataset(classes=[0, 1, 0, 0])
+        sampler = ClassBalancedSampler(ds, shuffle=False)
+        indices = [i for i in sampler]
+        classes = torch.tensor([ds.getitem_class(i) for i in indices])
+        _, counts = classes.unique(return_counts=True)
+        self.assertEqual([0, 2, 3, 1, 1, 1], indices)
+        self.assertEqual([3, 3], counts.tolist())
+
+    def test_sample_shuffle(self):
+        ds = ClassDataset(classes=[0, 1, 0, 0])
+        sampler = ClassBalancedSampler(ds, shuffle=True, seed=0)
+        indices = [i for i in sampler]
+        classes = torch.tensor([ds.getitem_class(i) for i in indices])
+        _, counts = classes.unique(return_counts=True)
+        self.assertEqual([1, 0, 3, 1, 2, 1], indices)
+        self.assertEqual([3, 3], counts.tolist())
