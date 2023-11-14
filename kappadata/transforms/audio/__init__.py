ae1a14ce855d4079223421168f156473c69e7645
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-11-14 12:57:54 +0100

    comment out audio

--- kappadata/transforms/audio/__init__.py
+++ kappadata/transforms/audio/__init__.py
@@ -1,4 +1,4 @@
-from .kd_fbank import KDFbank
+# from .kd_fbank import KDFbank
 from .kd_magnitude_jitter import KDMagnitudeJitter
 from .kd_roll import KDRoll
 from .kd_spec_augment import KDSpecAugment
